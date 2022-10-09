12c496f37c775f13dd27cbede1753e804f933e44
toxic-surya <113271427+akash20044@users.noreply.github.com>
2022-10-09 15:27:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -41,6 +41,7 @@ global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to o
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
+global.autoreact = true //auto reaction (true to on, false to off)
 
 //semt menu variants
 global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
